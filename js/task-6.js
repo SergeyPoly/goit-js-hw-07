3cc6a6a36ce7daad17b1fa32e5753b8784a5d2b4
Serhii-Polishchuk <serhii.polishchuk@skyhive.io>
2025-04-19 17:19:16 +0300

    add solution for tasks

---
+++ js/task-6.js
@@ -0,0 +1,47 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
+
+function createBoxes(amount) {
+  const boxes = [];
+  let size = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.push(box);
+    size += 10;
+  }
+
+  boxesContainer.append(...boxes);
+}
+
+const input = document.querySelector('#controls input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxesContainer = document.querySelector('#boxes');
+
+createBtn.addEventListener('click', () => {
+  const amount = parseInt(input.value.trim(), 10);
+
+  if (amount >= 1 && amount <= 100) {
+    destroyBoxes();
+    createBoxes(amount);
+    input.value = '';
+  } else {
+    alert('Please enter a number from 1 to 100');
+  }
+});
+
+destroyBtn.addEventListener('click', destroyBoxes);
+
+
+
